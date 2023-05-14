bf832d4e0933caea48dbcb9c6402b0b5b5aaa298
Caleb Danielsen <calebclientmanifold@gmail.com>
2023-05-14 11:43:54 +0200

    refactor: General AppDelegate cleanup

--- Bankey/AppDelegate.swift
+++ Bankey/AppDelegate.swift
@@ -28,15 +28,32 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         loginViewController.delegate = self
         onboardingContainerViewController.delegate = self
         
-        let vc = mainViewController
-        vc.setStatusBar()
+        displayLogin()
+
+        return true
+    }
+}
+
+// MARK: - UI
+extension AppDelegate {
+    private func displayLogin() {
+        setRootViewController(loginViewController)
+    }
+    
+    private func displayNextScreen() {
+        if LocalState.hasOnboarded {
+            prepMainView()
+            setRootViewController(mainViewController)
+        } else {
+            setRootViewController(onboardingContainerViewController)
+        }
+    }
+    
+    private func prepMainView() {
+        mainViewController.setStatusBar()
         
         UINavigationBar.appearance().isTranslucent = false
         UINavigationBar.appearance().backgroundColor = appColor
-        
-        window?.rootViewController = vc
-
-        return true
     }
 }
 
@@ -62,17 +79,14 @@ extension AppDelegate {
 // MARK: - Delegates
 extension AppDelegate: LoginViewControllerDelegate {
     func didLogin() {
-        if LocalState.hasOnboarded {
-            setRootViewController(mainViewController)
-        } else {
-            setRootViewController(onboardingContainerViewController)
-        }
+        displayNextScreen()
     }
 }
 
 extension AppDelegate: OnboardingContainerViewControllerDelegate {
     func didFinishOnbooarding() {
         LocalState.hasOnboarded = true
+        prepMainView()
         setRootViewController(mainViewController)
     }
 }
